792a00036b2fb878ee38a5847327aa9458820baa
user71424q <127671064+user71424q@users.noreply.github.com>
2023-10-24 16:39:52 +0300

    Лабораторная 3

---
+++ Лабораторная 3/1.py
@@ -0,0 +1,15 @@
+def search(item, item_list):
+    if item in item_list:
+        return item_list.index(item)
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search(find_item, items_list)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
